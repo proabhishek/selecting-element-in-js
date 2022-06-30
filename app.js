15cfe5f1a3ec90a7bc1b8279455fc915d7028ade
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2022-06-30 14:21:18 +0530

    selecting elements

---
+++ app.js
@@ -0,0 +1,15 @@
+// queryselector
+
+// heading = document.querySelector('h1');
+
+// getElementById()
+// heading = document.getElementById('third');
+
+// getElementsByClassName()
+// heading = document.getElementsByClassName('first');
+
+// getElementsByTagName()
+// heading = document.getElementsByTagName('h1');
+
+// getElementsByName()
+
